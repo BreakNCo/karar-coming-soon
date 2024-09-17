1172eab8fe5f005e4d9d605bbfca658a61ea1b93
sebastian-break-co <sebastian.break.co@gmail.com>
2024-09-17 15:52:14 +0530

    chore: update countdown date

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
 $(function () {
-    var now = new Date();
-    var day = now.getDate();
-    var month = now.getMonth();
-    var year = now.getFullYear() + 1;
-
-    var nextyear = month + '/' + day + '/' + year + ' 07:07:07';
-
     $('#timer').countdown({
-        date: nextyear, //TODO Date format: 07/27/2017 17:00:00
-        offset: +2,
+        date: '10/17/2024 17:00:00',
+        offset: +5.5,
         day: 'Day',
         days: 'Days'
     }, function () {
