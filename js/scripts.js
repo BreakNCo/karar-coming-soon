98bd8e8912c36af6cfb0d4cd2e47db58f65a4754
Joseph Tharakan <josh_tharakan@hotmail.com>
2024-09-22 13:45:27 +0530

    feat: to upgrade to bootstrap v5.3 and use bootstrap icons

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('#timer').countdown({
-        date: '10/17/2024 17:00:00',
-        offset: +5.5,
-        day: 'Day',
-        days: 'Days'
-    }, function () {
-    });
-});
